ed5478980933f967bc63efc01021c6ade45e27f9
Arya Priandana <gustiaryapriandana@gmail.com>
2026-02-11 10:23:13 +0700

    style:modify style exported excel for grouping data

--- app/Exports/DesaSheet.php
+++ app/Exports/DesaSheet.php
@@ -2,26 +2,58 @@
 
 namespace App\Exports;
 
-use Illuminate\Support\Facades\DB;
 use Maatwebsite\Excel\Concerns\FromCollection;
 use Maatwebsite\Excel\Concerns\WithHeadings;
 use Maatwebsite\Excel\Concerns\WithTitle;
+use Maatwebsite\Excel\Concerns\ShouldAutoSize;
+use Maatwebsite\Excel\Concerns\WithStyles;
+use PhpOffice\PhpSpreadsheet\Worksheet\Worksheet;
+use PhpOffice\PhpSpreadsheet\Style\Fill;
+use PhpOffice\PhpSpreadsheet\Style\Border;
+use Illuminate\Support\Facades\DB;
 
-class DesaSheet implements FromCollection, WithHeadings, WithTitle
+class DesaSheet implements
+    FromCollection,
+    WithHeadings,
+    WithTitle,
+    ShouldAutoSize,
+    WithStyles
 {
+    protected $rowCount;
+
     public function collection()
     {
-    return DB::table('pencatatan_usaha as pu')
-        ->join('nama_usaha as nu', 'pu.kode_nama_usaha', '=', 'nu.kode_nama_usaha')
-        ->join('desa as d', 'nu.kode_desa', '=', 'd.kode_desa')
-        ->select(
-            'd.kode_desa',
-            'd.nama_desa',
-            DB::raw('COUNT(pu.id) as total_usaha')
-        )
-        ->groupBy('d.kode_desa', 'd.nama_desa')
-        ->orderByDesc('total_usaha')
-        ->get();
+        $data = DB::table('pencatatan_usaha as pu')
+            ->join('nama_usaha as nu', 'pu.kode_nama_usaha', '=', 'nu.kode_nama_usaha')
+            ->join('desa as d', 'nu.kode_desa', '=', 'd.kode_desa')
+            ->select(
+                'd.kode_desa',
+                'd.nama_desa',
+                DB::raw('COUNT(pu.id) as total_usaha')
+            )
+            ->groupBy('d.kode_desa', 'd.nama_desa')
+            ->orderByDesc('total_usaha')
+            ->get();
+
+        $grandTotal = $data->sum('total_usaha');
+
+        // Baris kosong
+        $data->push((object)[
+            'kode_desa' => '',
+            'nama_desa' => '',
+            'total_usaha' => ''
+        ]);
+
+        // Grand Total
+        $data->push((object)[
+            'kode_desa' => '',
+            'nama_desa' => 'GRAND TOTAL',
+            'total_usaha' => $grandTotal
+        ]);
+
+        $this->rowCount = $data->count() + 1; // +1 header
+
+        return $data;
     }
 
     public function headings(): array
@@ -32,5 +64,39 @@ class DesaSheet implements FromCollection, WithHeadings, WithTitle
     public function title(): string
     {
         return 'Grouping Desa';
+    }
+
+    public function styles(Worksheet $sheet)
+    {
+        // HEADER STYLE
+        $sheet->getStyle('A1:C1')->applyFromArray([
+            'font' => ['bold' => true],
+            'fill' => [
+                'fillType' => Fill::FILL_SOLID,
+                'startColor' => ['rgb' => 'E5E7EB']
+            ],
+        ]);
+
+        $lastRow = $this->rowCount;
+
+        // GRAND TOTAL STYLE
+        $sheet->getStyle("A{$lastRow}:C{$lastRow}")
+            ->applyFromArray([
+                'font' => ['bold' => true],
+                'fill' => [
+                    'fillType' => Fill::FILL_SOLID,
+                    'startColor' => ['rgb' => 'FDE68A']
+                ],
+            ]);
+
+        // BORDER ALL
+        $sheet->getStyle("A1:C{$lastRow}")
+            ->applyFromArray([
+                'borders' => [
+                    'allBorders' => [
+                        'borderStyle' => Border::BORDER_THIN,
+                    ],
+                ],
+            ]);
     }
 }
--- app/Exports/PetugasSheet.php
+++ app/Exports/PetugasSheet.php
@@ -2,32 +2,96 @@
 
 namespace App\Exports;
 
-use Illuminate\Support\Facades\DB;
 use Maatwebsite\Excel\Concerns\FromCollection;
 use Maatwebsite\Excel\Concerns\WithHeadings;
 use Maatwebsite\Excel\Concerns\WithTitle;
-
-class PetugasSheet implements FromCollection, WithHeadings, WithTitle
+use Maatwebsite\Excel\Concerns\ShouldAutoSize;
+use Maatwebsite\Excel\Concerns\WithStyles;
+use PhpOffice\PhpSpreadsheet\Worksheet\Worksheet;
+use PhpOffice\PhpSpreadsheet\Style\Fill;
+use PhpOffice\PhpSpreadsheet\Style\Border;
+use Illuminate\Support\Facades\DB;
+class PetugasSheet implements
+    FromCollection,
+    WithHeadings,
+    WithTitle,
+    ShouldAutoSize,
+    WithStyles
 {
+    protected $rowCount;
+
     public function collection()
     {
-        return DB::table('pencatatan_usaha')
-            ->select('nama_petugas', DB::raw('COUNT(*) as total_usaha'))
+        $data = DB::table('pencatatan_usaha as pu')
+            ->select('nama_petugas', DB::raw('COUNT(pu.id) as total_usaha'))
             ->whereNotNull('nama_petugas')
             ->where('nama_petugas', '!=', '')
             ->groupBy('nama_petugas')
             ->orderByDesc('total_usaha')
             ->get();
+
+        $grandTotal = $data->sum('total_usaha');
+
+        // Tambah baris kosong
+        $data->push((object)[
+            'nama_petugas' => '',
+            'total_usaha' => ''
+        ]);
+
+        // Tambah Grand Total
+        $data->push((object)[
+            'nama_petugas' => 'GRAND TOTAL',
+            'total_usaha' => $grandTotal
+        ]);
+
+        $this->rowCount = $data->count() + 1; // +1 karena header
+
+        return $data;
     }
 
     public function headings(): array
     {
-        return ['Nama Petugas', 'Total Usaha'];
+        return ['Nama Pegawai', 'Total Usaha'];
     }
 
     public function title(): string
     {
         return 'Grouping Petugas';
     }
+
+    public function styles(Worksheet $sheet)
+    {
+        // HEADER STYLE
+        $sheet->getStyle('A1:B1')->applyFromArray([
+            'font' => ['bold' => true],
+            'fill' => [
+                'fillType' => Fill::FILL_SOLID,
+                'startColor' => ['rgb' => 'E5E7EB']
+            ],
+        ]);
+
+        // GRAND TOTAL STYLE (baris terakhir)
+        $lastRow = $this->rowCount;
+
+        $sheet->getStyle("A{$lastRow}:B{$lastRow}")
+            ->applyFromArray([
+                'font' => ['bold' => true],
+                'fill' => [
+                    'fillType' => Fill::FILL_SOLID,
+                    'startColor' => ['rgb' => 'FDE68A']
+                ],
+            ]);
+
+        // BORDER ALL
+        $sheet->getStyle("A1:B{$lastRow}")
+            ->applyFromArray([
+                'borders' => [
+                    'allBorders' => [
+                        'borderStyle' => Border::BORDER_THIN,
+                    ],
+                ],
+            ]);
+    }
 }
+
 
